7f95f8c907b650c57372f03e3f0d62a06aaf53fd
Nicholas Cheng <nchengyeeshen@gmail.com>
2022-10-06 21:11:23 +0800

    Format views.py

--- kkmnow/views.py
+++ kkmnow/views.py
@@ -1,26 +1,23 @@
-import os
+from threading import Thread
+
 import environ
+from django.core.cache import cache
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from django.core.cache import cache
-from django.conf import settings
-from django.core.cache.backends.base import DEFAULT_TIMEOUT
-
-from kkmnow.utils import cron_utils, triggers
+from kkmnow.utils import cron_utils
 
 from .models import KKMNowJSON, MetaJson
-
-from threading import Thread
 
 env = environ.Env()
 environ.Env.read_env()
 
+
 class KKMNOW(APIView):
     def post(self, *_):
-        Thread(target=cron_utils.data_operation, args=('UPDATE',)).start()
+        Thread(target=cron_utils.data_operation, args=("UPDATE",)).start()
         return Response(status=status.HTTP_200_OK)
 
     def get(self, request, *_):
@@ -33,56 +30,69 @@ class KKMNOW(APIView):
         else:
             return JsonResponse({}, safe=False)
 
+
 def handle_request(param_list):
     dbd_name = str(param_list["dashboard"][0])
     dbd_info = cache.get("META_" + dbd_name)
 
-    if not dbd_info :
-        dbd_info = MetaJson.objects.filter(dashboard_name=dbd_name).values('dashboard_meta')
+    if not dbd_info:
+        dbd_info = MetaJson.objects.filter(dashboard_name=dbd_name).values(
+            "dashboard_meta"
+        )
 
     params_req = []
 
     if len(dbd_info) > 0:
-        dbd_info = dbd_info if isinstance(dbd_info, dict) else dbd_info[0]["dashboard_meta"]
+        dbd_info = (
+            dbd_info if isinstance(dbd_info, dict) else dbd_info[0]["dashboard_meta"]
+        )
         params_req = dbd_info["required_params"]
 
     res = {}
     if all(p in param_list for p in params_req):
-        data = dbd_info['charts']
+        data = dbd_info["charts"]
 
-        if len(data) > 0 :
-            for k, v in data.items() :
-                api_type = v['api_type']
-                api_params = v['api_params']
+        if len(data) > 0:
+            for k, v in data.items():
+                api_type = v["api_type"]
+                api_params = v["api_params"]
                 cur_chart_data = cache.get(dbd_name + "_" + k)
 
-                if not cur_chart_data :
-                    cur_chart_data = KKMNowJSON.objects.filter(dashboard_name=dbd_name, chart_name=k).values('chart_data')[0]['chart_data']                    
+                if not cur_chart_data:
+                    cur_chart_data = KKMNowJSON.objects.filter(
+                        dashboard_name=dbd_name, chart_name=k
+                    ).values("chart_data")[0]["chart_data"]
                     cache.set(dbd_name + "_" + k, cur_chart_data)
 
-                if api_type == 'static' : 
+                if api_type == "static":
                     res[k] = cur_chart_data
-                else : 
+                else:
                     if len(api_params) > 0:
-                        cur_chart_data = get_nested_data(api_params, param_list, cur_chart_data)
-                    
+                        cur_chart_data = get_nested_data(
+                            api_params, param_list, cur_chart_data
+                        )
+
                     if len(cur_chart_data) > 0:
                         res[k] = cur_chart_data
     return res
 
-'''
+
+"""
 Slices dictionary,
 based on keys within dictionary
-'''
+"""
 
-def get_nested_data(api_params, param_list, data) :
+
+def get_nested_data(api_params, param_list, data):
     for a in api_params:
         if a in param_list:
-            key = param_list[a][0] if "__FIXED__" not in a else a.replace("__FIXED__", "")
+            key = (
+                param_list[a][0] if "__FIXED__" not in a else a.replace("__FIXED__", "")
+            )
             if key in data:
                 data = data[key]
         else:
             data = {}
             break
-    
+
     return data
